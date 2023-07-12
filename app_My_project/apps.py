0e40d2997a3abbc7de99c508c0bfacc764b291e4
matildochka <mathildochka@mail.ru>
2023-07-12 09:32:32 +0300

    Первая публикация

---
+++ app_My_project/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppMyProjectConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_My_project'
---
+++ app_My_project/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def fun(request):
+    return HttpResponse('Урррррраааааа!')
+
+# Create your views here.
